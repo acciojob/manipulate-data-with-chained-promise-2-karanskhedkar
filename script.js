185871707eff568cb8f2bddc02ecc94f01e629bc
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-22 12:51:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const initialArray = [1, 2, 3, 4];
+
+const filterOdds = (data) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      data = data.filter((ele) => ele % 2 === 0);
+      resolve(data);
+    }, 1000);
+  });
+};
+
+const applyMultiply = (data) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      data = data.map((ele) => ele * 2);
+      resolve(data);
+    }, 2000);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const outputDiv = document.getElementById("output");
+
+  filterOdds(initialArray)
+    .then((res) => {
+      outputDiv.innerText = res.join(",");
+      return applyMultiply(res);
+    })
+    .then((res) => {
+      outputDiv.innerText = res.join(",");
+    });
+});
